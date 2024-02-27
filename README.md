34faff74bea2328a08df63c26bced2e581cd8d01
BierOnBits <eduardo.bier@referenceit.com.br>
2024-02-27 12:07:06 -0300

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# bos_backend_codemodule
-bos_backend_codemodule
+## Code Module Resource - módulo para Bos Backend
+
+# Descrição
+
+Módulo que possibilita o compartilhamento de códigos (criação de módulos no backend do módulo Bos Backend). Utilizado para criar códigos reutilizáveis e organização de estrutura de projeto.
+
+# História de versão
+
+eduardo.bitec 27-fev-2024 - Publicando primeira versão do módulo Code Module, para uso na framework Bier On Stack (BOS).
---
+++ index.js
@@ -0,0 +1,31 @@
+var Resource = require("bos_backend/lib/resource"),
+    util = require("util"),
+    fs = require("fs"),
+    path = require("path");
+
+var resourcesPath = fs.realpathSync("./") + "/resources/";
+
+function CodeResource(moduleName) {
+    Resource.apply(this, arguments);
+
+    this.on("changed", function (config) {
+        delete require.cache[require.resolve(resourcesPath + moduleName)];
+    });
+}
+util.inherits(CodeResource, Resource);
+
+CodeResource.label = "Code";
+CodeResource.events = ["index"];
+
+module.exports = CodeResource;
+
+CodeResource.prototype.clientGeneration = false;
+
+CodeResource.prototype.handle = function (ctx, next) {
+    next();
+};
+
+global.requireModule = function (module) {
+    var safeModule = path.normalize(module).replace(/^(\.\.[\/\\])+/, "");
+    return require(resourcesPath + safeModule);
+};
